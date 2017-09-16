ca576ccd7aa38e7bdd51ce5fae75ab08c38703de
dev7355608 <dev7355608@gmail.com>
2017-09-17 01:35:22 +0200

    Change to auto generated files

--- app.py
+++ app.py
@@ -32,13 +32,41 @@ def extract(data, *keys, default=None):
 
 print('Please read the instructions carefully!\n')
 
+settings_path = os.path.join(application_path, 'settings.json')
+
+if os.path.isfile(settings_path):
+    with open(settings_path) as f:
+        settings = json.load(f)
+else:
+    settings = dict(port=54237,
+                    mat_monitorgamma=2,
+                    mat_monitorgamma_tv_enabled=0)
+
+    with open(settings_path, mode='w') as f:
+        json.dump(settings, f, indent=2, sort_keys=True)
+
+port = settings.get('port')
+
+with open(os.path.join(application_path,
+                       'gamestate_integration_dont_blind_me.cfg'), 'w') as f:
+    f.write('''"csgo_dont_blind_me"
+{{
+    "uri" "http://127.0.0.1:{}"
+    "timeout"   "1.0"
+    "buffer"    "0.0"
+    "throttle"  "0.0"
+    "heartbeat" "300.0"
+    "data"
+    {{
+        "player_id"    "1"
+        "player_state" "1"
+    }}
+}}'''.format(port))
+
 print("Don't forget to copy gamestate_integration_dont_blind_me.cfg into\n"
       "    ...\\Steam\\userdata\\________\\730\\local\\cfg, or\n"
       "    ...\\Steam\\steamapps\\common\\Counter-Strike Global Offensive\\"
       "csgo\\cfg!\n")
-
-with open(os.path.join(application_path, 'settings.json')) as f:
-    settings = json.load(f)
 
 mat_monitorgamma = settings.get('mat_monitorgamma')
 mat_monitorgamma_tv_enabled = settings.get('mat_monitorgamma_tv_enabled')
@@ -49,6 +77,12 @@ if mat_monitorgamma_tv_enabled:
 else:
     gamma = 2.2 / mat_monitorgamma
     remap = (0, 255)
+
+print("Don't forget to set your preferred gamma in settings.json! "
+      "Currently set to:\n"
+      "    mat_monitorgamma\t\t {:3.2f}\n"
+      "    mat_monitorgamma_tv_enabled\t {}\n".format(
+          mat_monitorgamma, int(mat_monitorgamma_tv_enabled)))
 
 context = GammaContext()
 
@@ -89,15 +123,17 @@ if platform.system() == 'Windows':
             else:
                 f.write('dword:{:08x}'.format(gamma_range))
 
+        with open(os.path.join(application_path,
+                               'set_max_gamma_range.reg'), mode='w') as f:
+            f.write('Windows Registry Editor Version 5.00\n\n')
+            f.write('[{}]\n'.format(icm))
+            f.write('"GdiIcmGammaRange"=dword:{:08x}'.format(256))
+
         print('Gamma range is currently limited. To fix that, please\n'
               '    (1) run set_max_gamma_range.reg, then\n'
               '    (2) reboot PC for it to take effect!')
         sys.exit()
 
-print("Don't forget to set your preferred gamma! Currently set to:\n"
-      "    mat_monitorgamma\t\t {:3.2f}\n"
-      "    mat_monitorgamma_tv_enabled\t {}\n".format(
-          mat_monitorgamma, int(mat_monitorgamma_tv_enabled)))
 
 print("Don't forget to set the launch option -nogammaramp!\n")
 
@@ -111,7 +147,6 @@ log = logging.getLogger('werkzeug')
 log.setLevel(logging.ERROR)
 
 app = Flask(__name__)
-port = settings.get('port')
 
 
 @app.route('/', methods=['POST'])
