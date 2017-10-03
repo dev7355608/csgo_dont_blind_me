3379d49a26ba43b342c14089168a73aa575c3919
dev7355608 <dev7355608@gmail.com>
2017-10-03 17:24:21 +0200

    Add function to generate gamma ramps

--- gamma/__init__.py
+++ gamma/__init__.py
@@ -1,5 +1,6 @@
 from .context import Context, ContextError
 from .calibration import read_icc_ramp
+from .ramp import generate_ramp
 
 
-__all__ = ['Context', 'ContextError', 'read_icc_ramp']
+__all__ = ['Context', 'ContextError', 'generate_ramp', 'read_icc_ramp']
